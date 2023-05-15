51a2e54190bffd3a3d32d753b93578a4d24b3444
Engrid Bruna <87833581+01Bruna11@users.noreply.github.com>
2023-05-15 12:56:48 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+//import logo from './logo.svg';
+import './App.css';
+//importar o style.css
+//import Pai from './atividade01/questao01/01Pai';
+//import Avo from "./atividade02/questao02/Avo";
+//import SaveData from "./components/07Storage/MyStorage";
+//import Pai from "./components/08PaiFilho/Pai"
+//import Contador from "./components/09Estados/Contador"
+//import Cidades from "./atividade03/questao01"
+//import PokemonAxios from './components/10Axios/PokeonAxios';
+//import MeuIndex from './components/11Redux/v2/MeuIndex';
+//import MinhaPromise from './components/12Promises/MinhaPromise';
+//import MyRefs from './components/13Refs/MyRefs';
+//import Questao02 from './Prova01/Questao02';
+//import Questao01 from './Prova01/Questao01';
+import Questao02 from './Prova01/Questao02';
+import Questao03 from './Prova01/Questao03';
+function App() {
+  return (
+    /*
+    //01MeusDados
+    <div className='MeusDados'>
+      <MeusDados/>
+    </div>
+    */
+    //02MeusDados
+    /*<div className='MeusDados'> 
+      <MeusDados 
+      nome = " Engrid Bruna"
+      curso = " Ciência da Computação"
+      universidade = " UFC - Quixadá"
+      />
+    </div>
+    */
+ /* <div  className="App">
+    <Questao02/>
+    <Questao03/>
+  </div> */
+  <div  className="App">
+    <Questao02/>
+    <Questao03/>
+  </div>
+
+  );
+   
+}
+
+export default App;
